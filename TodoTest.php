9f3a54c7e7f2615ac112ef18995be0ff394d3c7d
hannah.yang <hannah.yang@104.com.tw>
2022-04-22 13:48:46 +0800

    新增PSR12

--- TodoTest.php
+++ TodoTest.php
@@ -2,14 +2,17 @@
 
 require_once 'class.php';
 
-class TodoTest extends \PHPUnit\Framework\TestCase {
+use Hannah\TodoList;
+use PHPUnit\Framework\TestCase;
+
+class TodoTest extends TestCase {
 
     /**
      * @test
      * @testdox 當項目清單是空的時候，新增一個清單項目「買菜」，優先權「無」，類別「工作」，項目清單應該只有一個「買菜」項目
      */
     public function shouldShowOneItemWhenAddItemIntoEmptyList() {
-        $target = new todoList();
+        $target = new TodoList();
         $target->addItem('買菜');
 
         $actual = $target->showList();
@@ -22,7 +25,7 @@ class TodoTest extends \PHPUnit\Framework\TestCase {
      * @testdox 當項目清單有「買菜」項目時，新增一個清單項目「看書」，優先權「低」，類別「無」，項目清單應該要有「買菜」「看書」兩個項目
      */
     public function shouldShowTwoItemWhenAddItemIntoList() {
-        $target = new todoList();
+        $target = new TodoList();
         $target->addItem('看書');
 
         $actual = $target->showList();
@@ -35,7 +38,7 @@ class TodoTest extends \PHPUnit\Framework\TestCase {
      * @testdox 當項目清單有「買菜」「看書」「寫扣」「澆花」項目時，將「看書」更新為「看電視」，項目清單應該要有「買菜」「看電視」「寫扣」「澆花」四個項目
      */
     public function shouldShowUpdateItemWhenUpdateItem() {
-        $target = new todoList();
+        $target = new TodoList();
         $target->addItem('買菜');
         $target->addItem('看書');
         $target->addItem('寫扣');
@@ -52,7 +55,7 @@ class TodoTest extends \PHPUnit\Framework\TestCase {
      * @testdox 當項目清單有「買菜」「看電視」「寫扣」「澆花」項目時，刪除「買菜」項目，項目清單應該只會出現「看電視」「寫扣」「澆花」三個項目
      */
     public function shouldShowThreeItemWhenDelItem() {
-        $target = new todoList();
+        $target = new TodoList();
         $target->addItem('買菜');
         $target->addItem('看書');
         $target->addItem('寫扣');
@@ -70,7 +73,7 @@ class TodoTest extends \PHPUnit\Framework\TestCase {
      * @testdox 項目清單有「看電視」「寫扣」「澆花」項目時，更新「看電視」狀態「已完成」，「看電視」狀態應該會是「已完成」
      */
     // public function shouldShowItemStatWhenUpdateItemStat() {
-    //     $target = new todoList();
+    //     $target = new TodoList();
     //     $target->addItem('買菜');
     //     $target->addItem('看書');
     //     $target->addItem('寫扣');
@@ -90,7 +93,7 @@ class TodoTest extends \PHPUnit\Framework\TestCase {
      */
     public function shouldShowUpdateEmptyItemWhenUpdateItem() {
         // $this->expectException(Exception::class);
-        $target = new todoList();
+        $target = new TodoList();
         $target->addItem('買菜');
         $target->addItem('看書');
         $target->addItem('寫扣');
@@ -109,7 +112,7 @@ class TodoTest extends \PHPUnit\Framework\TestCase {
      */
     public function shouldShowOriEmptyItemWhenUpdateItem() {
         // $this->expectException(Exception::class);
-        $target = new todoList();
+        $target = new TodoList();
         $target->addItem('買菜');
         $target->addItem('看書');
         $target->addItem('寫扣');
@@ -128,7 +131,7 @@ class TodoTest extends \PHPUnit\Framework\TestCase {
      */
     public function shouldShowEmptyItemWhenUpdateEmptytem() {
         // $this->expectException(Exception::class);
-        $target = new todoList();
+        $target = new TodoList();
         $target->addItem('買菜');
         $target->addItem('看書');
         $target->addItem('寫扣');
@@ -147,7 +150,7 @@ class TodoTest extends \PHPUnit\Framework\TestCase {
      */
     public function shouldShowUpdateItemWhenUpdateErrorItem() {
         // $this->expectException(Exception::class);
-        $target = new todoList();
+        $target = new TodoList();
         $target->addItem('買菜');
         $target->addItem('看書');
         $target->addItem('寫扣');
@@ -166,7 +169,7 @@ class TodoTest extends \PHPUnit\Framework\TestCase {
      */
     public function shouldShowEmptyItemWhenDelEmptyItem() {
         // $this->expectException(Exception::class);
-        $target = new todoList();
+        $target = new TodoList();
         $target->addItem('買菜');
         $target->addItem('看書');
         $target->addItem('寫扣');
@@ -184,7 +187,7 @@ class TodoTest extends \PHPUnit\Framework\TestCase {
      */
     public function shouldShowDelItemWhenDelErrorItem() {
         // $this->expectException(Exception::class);
-        $target = new todoList();
+        $target = new TodoList();
         $target->addItem('買菜');
         $target->addItem('看書');
         $target->addItem('寫扣');
@@ -201,7 +204,7 @@ class TodoTest extends \PHPUnit\Framework\TestCase {
      * @testdox 當項目清單有「買菜」項目時，新增一個清單項目「」，應該會出現「新增的item不得為空」的錯誤訊息
      */
     public function shouldShowAddEmptyItemWhenAddItem() {
-        $target = new todoList();
+        $target = new TodoList();
         $actual = $target->addItem('');
 
         // $actual = $target->showList();
@@ -214,7 +217,7 @@ class TodoTest extends \PHPUnit\Framework\TestCase {
      * @testdox 當項目清單有「買菜」項目時，新增一個清單項目「寫扣」，優先權「未定義」，應該會出現「優先權錯誤」的錯誤訊息
      */
     public function shouldShowAddErrorPriorityItemWhenAddItem() {
-        $target = new todoList();
+        $target = new TodoList();
         $actual = $target->addItem('寫扣', 4);
 
         // $actual = $target->showList();
@@ -227,7 +230,7 @@ class TodoTest extends \PHPUnit\Framework\TestCase {
      * @testdox 當項目清單有「買菜」項目時，新增一個清單項目「寫扣」，優先權「中」，類別「不存在」，應該會出現「類別錯誤」的錯誤訊息
      */
     public function shouldShowAddErrorClassItemWhenAddItem() {
-        $target = new todoList();
+        $target = new TodoList();
         $actual = $target->addItem('寫扣', 2, 4);
 
         // $actual = $target->showList();
--- class.php
+++ class.php
@@ -1,154 +1,46 @@
 <?php
-    class todoList extends Exception {
 
-        // 待辦清單事項
-        public $items;
-        // 待辦清單事項狀態（0：未完成，1：已完成）
-        public $itemsStat;
-        // 待辦清單事項優先權（0：無，1：低，2：中，3：高）
-        public $itemsPriority;
-        // 待辦清單事項類別（0：無，1：私人，2：工作，3：家庭）
-        public $itemsClass;
-        // 錯誤訊息
-        public $errorMsg;
+namespace Hannah;
 
-        public function __construct() {
-            $this->items = array();
-            $this->itemsStat = array();
-            $this->itemsPriority = array();
-            $this->itemsClass = array();
-            $this->errorMsg = '';
-        }
+use Exception;
 
-        /**
-         * @params string $item
-         * @params int $priority
-         * @params int $class
-         * @return true, others
-         */
-        public function addItem($item, $priority = 0, $class = 0) {
-            if($item !== '' && $priority <= 3 && $class <=3) {
-                $this->items[] = $item;
-                $this->itemsStat[] = 0;
-                $this->itemsPriority[] = $priority;
-                $this->itemsClass[] = $class;
+class TodoList extends Exception
+{
+    // 待辦清單事項
+    public $items;
+    // 待辦清單事項狀態（0：未完成，1：已完成）
+    public $itemsStat;
+    // 待辦清單事項優先權（0：無，1：低，2：中，3：高）
+    public $itemsPriority;
+    // 待辦清單事項類別（0：無，1：私人，2：工作，3：家庭）
+    public $itemsClass;
+    // 錯誤訊息
+    public $errorMsg;
 
-                // echo 'addResult: '.PHP_EOL;
+    public function __construct()
+    {
+        $this->items = array();
+        $this->itemsStat = array();
+        $this->itemsPriority = array();
+        $this->itemsClass = array();
+        $this->errorMsg = '';
+    }
 
-                // echo 'items: '.PHP_EOL;
-                // print_r($this->items);
+    /**
+     * @params string $item
+     * @params int $priority
+     * @params int $class
+     * @return true, others
+     */
+    public function addItem($item, $priority = 0, $class = 0)
+    {
+        if ($item !== '' && $priority <= 3 && $class <= 3) {
+            $this->items[] = $item;
+            $this->itemsStat[] = 0;
+            $this->itemsPriority[] = $priority;
+            $this->itemsClass[] = $class;
 
-                // echo 'itemsStat: '.PHP_EOL;
-                // print_r($this->itemsStat);
-
-                // echo 'itemsPriority: '.PHP_EOL;
-                // print_r($this->itemsPriority);
-
-                // echo 'itemsClass: '.PHP_EOL;
-                // print_r($this->itemsClass);
-                return true;
-            } else {
-                if($item == '')     return '新增的item不得為空';
-                // if($item == '')     $this->errorMsg = '新增的item不得為空'.PHP_EOL;
-                if($priority > 3)   return '優先權錯誤';
-                // if($priority > 3)   $this->errorMsg = '優先權錯誤'.PHP_EOL;
-                if($class > 3)      return '類別錯誤';
-                // if($class > 3)      $this->errorMsg = '類別錯誤'.PHP_EOL;
-                throw new Exception($this->errorMsg);
-            }
-        }
-
-        /**
-         * @params string $newItem
-         * @params string $oriItem
-         * @return true, others
-         */
-        public function updateItem($newItem, $oriItem) {
-            if(in_array($oriItem, $this->items) && $newItem !== '' && $oriItem !== '') {
-                $key = array_search($oriItem, $this->items);
-                $this->items[$key] = $newItem;
-
-                // echo 'updateResult: '.PHP_EOL;
-
-                // echo 'items: '.PHP_EOL;
-                // print_r($this->items);
-
-                // echo 'itemsStat: '.PHP_EOL;
-                // print_r($this->itemsStat);
-
-                // echo 'itemsPriority: '.PHP_EOL;
-                // print_r($this->itemsPriority);
-
-                // echo 'itemsClass: '.PHP_EOL;
-                // print_r($this->itemsClass);
-                return true;
-            } else if($newItem == '') {
-                // $this->errorMsg = '新item不得為空'.PHP_EOL;
-                // throw new Exception($this->errorMsg);
-                return '新item不得為空';
-            } else if($oriItem == '') {
-                // $this->errorMsg = '原item不得為空'.PHP_EOL;
-                // throw new Exception($this->errorMsg);
-                return '原item不得為空';
-            } else {
-                // $this->errorMsg = 'updateResult: '.PHP_EOL.'找不到名為'.$oriItem.'的待辦事項'.PHP_EOL;
-                // throw new Exception($this->errorMsg);
-                return '找不到名為'.$oriItem.'的待辦事項';
-            }
-        }
-
-        /**
-         * @params string $item
-         * @return true, others
-         */
-        public function delItem($item) {
-            if(in_array($item, $this->items) && $item !== '') {
-                $key = array_search($item, $this->items);
-
-                unset($this->items[$key]);
-                $this->items = array_values($this->items);
-
-                unset($this->itemsStat[$key]);
-                $this->itemsStat = array_values($this->itemsStat);
-
-                unset($this->itemsPriority[$key]);
-                $this->itemsPriority = array_values($this->itemsPriority);
-
-                unset($this->itemsClass[$key]);
-                $this->itemsClass = array_values($this->itemsClass);
-
-                // echo 'delResult: '.PHP_EOL;
-
-                // echo 'items: '.PHP_EOL;
-                // print_r($this->items);
-
-                // echo 'itemsStat: '.PHP_EOL;
-                // print_r($this->itemsStat);
-
-                // echo 'itemsPriority: '.PHP_EOL;
-                // print_r($this->itemsPriority);
-
-                // echo 'itemsClass: '.PHP_EOL;
-                // print_r($this->itemsClass);
-                return true;
-            } else if($item == '') {
-                // $this->errorMsg = '刪除的item不得為空'.PHP_EOL;
-                // throw new Exception($this->errorMsg);
-                return '刪除的item不得為空';
-            } else {
-                // $this->errorMsg = 'delResult: '.PHP_EOL.'找不到名為'.$item.'的待辦事項'.PHP_EOL;
-                // throw new Exception($this->errorMsg);
-                return '找不到名為'.$item.'的待辦事項';
-            }
-        }
-
-        /**
-         * @return items
-         */
-        public function showList(){
-            return $this->items;
-
-            // echo 'showListResult: '.PHP_EOL;
+            // echo 'addResult: '.PHP_EOL;
 
             // echo 'items: '.PHP_EOL;
             // print_r($this->items);
@@ -161,101 +53,228 @@
 
             // echo 'itemsClass: '.PHP_EOL;
             // print_r($this->itemsClass);
-        }
-
-        /**
-         * @params int $key
-         * @params int $stat
-         * @return true, others
-         */
-        public function updateItemStat($key, $stat = 0){
-            if(array_key_exists($key, $this->items) && $stat <= 1) {
-                $this->itemsStat[$key] = $stat;
-
-                echo 'updateStatResult: '.PHP_EOL;
-
-                echo 'items: '.PHP_EOL;
-                print_r($this->items);
-
-                echo 'itemsStat: '.PHP_EOL;
-                print_r($this->itemsStat);
-
-                echo 'itemsPriority: '.PHP_EOL;
-                print_r($this->itemsPriority);
-
-                echo 'itemsClass: '.PHP_EOL;
-                print_r($this->itemsClass);
-            } else if($stat > 1) {
-                $this->errorMsg = '狀態錯誤'.PHP_EOL;
-                throw new Exception($this->errorMsg);
-            } else {
-                $this->errorMsg = 'updateStatResult: '.PHP_EOL.'找不到key為'.$key.'的待辦事項'.PHP_EOL;
-                throw new Exception($this->errorMsg);
+            return true;
+        } else {
+            if ($item == '') {
+                return '新增的item不得為空';
             }
-        }
-
-        /**
-         * @params int $key
-         * @params int $priority
-         * @return true, others
-         */
-        public function updateItemPriority($key, $priority = 0){
-            if(array_key_exists($key, $this->items) && $priority <= 3) {
-                $this->itemsPriority[$key] = $priority;
-
-                echo 'updatePriorityResult: '.PHP_EOL;
-
-                echo 'items: '.PHP_EOL;
-                print_r($this->items);
-
-                echo 'itemsStat: '.PHP_EOL;
-                print_r($this->itemsStat);
-
-                echo 'itemsPriority: '.PHP_EOL;
-                print_r($this->itemsPriority);
-
-                echo 'itemsClass: '.PHP_EOL;
-                print_r($this->itemsClass);
-            } else if($priority > 3) {
-                $this->errorMsg = '優先權錯誤'.PHP_EOL;
-                throw new Exception($this->errorMsg);
-            } else {
-                $this->errorMsg = 'updatePriorityResult: '.PHP_EOL.'找不到key為'.$key.'的待辦事項'.PHP_EOL;
-                throw new Exception($this->errorMsg);
+            // if($item == '')     $this->errorMsg = '新增的item不得為空'.PHP_EOL;
+            if ($priority > 3) {
+                return '優先權錯誤';
             }
-        }
-
-        /**
-         * @params int $key
-         * @params int $class
-         * @return true, others
-         */
-        public function updateItemClass($key, $class = 0){
-            if(array_key_exists($key, $this->items) && $class <= 3) {
-                $this->itemsClass[$key] = $class;
-
-                echo 'updateClassResult: '.PHP_EOL;
-
-                echo 'items: '.PHP_EOL;
-                print_r($this->items);
-
-                echo 'itemsStat: '.PHP_EOL;
-                print_r($this->itemsStat);
-
-                echo 'itemsPriority: '.PHP_EOL;
-                print_r($this->itemsPriority);
-
-                echo 'itemsClass: '.PHP_EOL;
-                print_r($this->itemsClass);
-            } else if($class > 3) {
-                $this->errorMsg = '類別錯誤'.PHP_EOL;
-                throw new Exception($this->errorMsg);
-            } else {
-                $this->errorMsg = 'updateClassResult: '.PHP_EOL.'找不到key為'.$key.'的待辦事項'.PHP_EOL;
-                throw new Exception($this->errorMsg);
+            // if($priority > 3)   $this->errorMsg = '優先權錯誤'.PHP_EOL;
+            if ($class > 3) {
+                return '類別錯誤';
             }
+            // if($class > 3)      $this->errorMsg = '類別錯誤'.PHP_EOL;
+            // throw new Exception($this->errorMsg);
         }
     }
+
+    /**
+     * @params string $newItem
+     * @params string $oriItem
+     * @return true, others
+     */
+    public function updateItem($newItem, $oriItem)
+    {
+        if (in_array($oriItem, $this->items) && $newItem !== '' && $oriItem !== '') {
+            $key = array_search($oriItem, $this->items);
+            $this->items[$key] = $newItem;
+
+            // echo 'updateResult: '.PHP_EOL;
+
+            // echo 'items: '.PHP_EOL;
+            // print_r($this->items);
+
+            // echo 'itemsStat: '.PHP_EOL;
+            // print_r($this->itemsStat);
+
+            // echo 'itemsPriority: '.PHP_EOL;
+            // print_r($this->itemsPriority);
+
+            // echo 'itemsClass: '.PHP_EOL;
+            // print_r($this->itemsClass);
+            return true;
+        } elseif ($newItem == '') {
+            // $this->errorMsg = '新item不得為空'.PHP_EOL;
+            // throw new Exception($this->errorMsg);
+            return '新item不得為空';
+        } elseif ($oriItem == '') {
+            // $this->errorMsg = '原item不得為空'.PHP_EOL;
+            // throw new Exception($this->errorMsg);
+            return '原item不得為空';
+        } else {
+            // $this->errorMsg = 'updateResult: '.PHP_EOL.'找不到名為'.$oriItem.'的待辦事項'.PHP_EOL;
+            // throw new Exception($this->errorMsg);
+            return '找不到名為' . $oriItem . '的待辦事項';
+        }
+    }
+
+    /**
+     * @params string $item
+     * @return true, others
+     */
+    public function delItem($item)
+    {
+        if (in_array($item, $this->items) && $item !== '') {
+            $key = array_search($item, $this->items);
+
+            unset($this->items[$key]);
+            $this->items = array_values($this->items);
+
+            unset($this->itemsStat[$key]);
+            $this->itemsStat = array_values($this->itemsStat);
+
+            unset($this->itemsPriority[$key]);
+            $this->itemsPriority = array_values($this->itemsPriority);
+
+            unset($this->itemsClass[$key]);
+            $this->itemsClass = array_values($this->itemsClass);
+
+            // echo 'delResult: '.PHP_EOL;
+
+            // echo 'items: '.PHP_EOL;
+            // print_r($this->items);
+
+            // echo 'itemsStat: '.PHP_EOL;
+            // print_r($this->itemsStat);
+
+            // echo 'itemsPriority: '.PHP_EOL;
+            // print_r($this->itemsPriority);
+
+            // echo 'itemsClass: '.PHP_EOL;
+            // print_r($this->itemsClass);
+            return true;
+        } elseif ($item == '') {
+            // $this->errorMsg = '刪除的item不得為空'.PHP_EOL;
+            // throw new Exception($this->errorMsg);
+            return '刪除的item不得為空';
+        } else {
+            // $this->errorMsg = 'delResult: '.PHP_EOL.'找不到名為'.$item.'的待辦事項'.PHP_EOL;
+            // throw new Exception($this->errorMsg);
+            return '找不到名為' . $item . '的待辦事項';
+        }
+    }
+
+    /**
+     * @return items
+     */
+    public function showList()
+    {
+        return $this->items;
+
+        // echo 'showListResult: '.PHP_EOL;
+
+        // echo 'items: '.PHP_EOL;
+        // print_r($this->items);
+
+        // echo 'itemsStat: '.PHP_EOL;
+        // print_r($this->itemsStat);
+
+        // echo 'itemsPriority: '.PHP_EOL;
+        // print_r($this->itemsPriority);
+
+        // echo 'itemsClass: '.PHP_EOL;
+        // print_r($this->itemsClass);
+    }
+
+    /**
+     * @params int $key
+     * @params int $stat
+     * @return true, others
+     */
+    public function updateItemStat($key, $stat = 0)
+    {
+        if (array_key_exists($key, $this->items) && $stat <= 1) {
+            $this->itemsStat[$key] = $stat;
+
+            echo 'updateStatResult: ' . PHP_EOL;
+
+            echo 'items: ' . PHP_EOL;
+            print_r($this->items);
+
+            echo 'itemsStat: ' . PHP_EOL;
+            print_r($this->itemsStat);
+
+            echo 'itemsPriority: ' . PHP_EOL;
+            print_r($this->itemsPriority);
+
+            echo 'itemsClass: ' . PHP_EOL;
+            print_r($this->itemsClass);
+        } elseif ($stat > 1) {
+            $this->errorMsg = '狀態錯誤' . PHP_EOL;
+            throw new Exception($this->errorMsg);
+        } else {
+            $this->errorMsg = 'updateStatResult: ' . PHP_EOL . '找不到key為' . $key . '的待辦事項' . PHP_EOL;
+            throw new Exception($this->errorMsg);
+        }
+    }
+
+    /**
+     * @params int $key
+     * @params int $priority
+     * @return true, others
+     */
+    public function updateItemPriority($key, $priority = 0)
+    {
+        if (array_key_exists($key, $this->items) && $priority <= 3) {
+            $this->itemsPriority[$key] = $priority;
+
+            echo 'updatePriorityResult: ' . PHP_EOL;
+
+            echo 'items: ' . PHP_EOL;
+            print_r($this->items);
+
+            echo 'itemsStat: ' . PHP_EOL;
+            print_r($this->itemsStat);
+
+            echo 'itemsPriority: ' . PHP_EOL;
+            print_r($this->itemsPriority);
+
+            echo 'itemsClass: ' . PHP_EOL;
+            print_r($this->itemsClass);
+        } elseif ($priority > 3) {
+            $this->errorMsg = '優先權錯誤' . PHP_EOL;
+            throw new Exception($this->errorMsg);
+        } else {
+            $this->errorMsg = 'updatePriorityResult: ' . PHP_EOL . '找不到key為' . $key . '的待辦事項' . PHP_EOL;
+            throw new Exception($this->errorMsg);
+        }
+    }
+
+    /**
+     * @params int $key
+     * @params int $class
+     * @return true, others
+     */
+    public function updateItemClass($key, $class = 0)
+    {
+        if (array_key_exists($key, $this->items) && $class <= 3) {
+            $this->itemsClass[$key] = $class;
+
+            echo 'updateClassResult: ' . PHP_EOL;
+
+            echo 'items: ' . PHP_EOL;
+            print_r($this->items);
+
+            echo 'itemsStat: ' . PHP_EOL;
+            print_r($this->itemsStat);
+
+            echo 'itemsPriority: ' . PHP_EOL;
+            print_r($this->itemsPriority);
+
+            echo 'itemsClass: ' . PHP_EOL;
+            print_r($this->itemsClass);
+        } elseif ($class > 3) {
+            $this->errorMsg = '類別錯誤' . PHP_EOL;
+            throw new Exception($this->errorMsg);
+        } else {
+            $this->errorMsg = 'updateClassResult: ' . PHP_EOL . '找不到key為' . $key . '的待辦事項' . PHP_EOL;
+            throw new Exception($this->errorMsg);
+        }
+    }
+}
 
     // $todoList = new todoList();
 
