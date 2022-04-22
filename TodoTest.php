3d1671c8e6f3886c7103fe9b74730e5cbd76fd44
hannah.yang <hannah.yang@104.com.tw>
2022-04-22 10:59:49 +0800

    Add testcase2

--- TodoTest.php
+++ TodoTest.php
@@ -16,4 +16,17 @@ class TodoTest extends \PHPUnit\Framework\TestCase {
 
         $this->assertSame(['買菜'], $actual);
     }
+
+    /**
+     * @test
+     * @testdox 當項目清單有「買菜」項目時，新增一個清單項目「看書」，優先權「低」，類別「無」，項目清單應該要有「買菜」「看書」兩個項目
+     */
+    public function shouldShowTwoItemWhenAddItemIntoList() {
+        $target = new todoList();
+        $target->addItem('看書');
+
+        $actual = $target->showList();
+
+        $this->assertSame(['看書'], $actual);
+    }
 }
